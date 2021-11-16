c0e2bd2549752c27a390ec6e2fb1322d49daddbd
Thiago Lemos <lemoos.th@gmail.com>
2021-11-15 23:03:01 -0300

    Exibindo resultado

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 (function () {
-  const quantidadeNumerosSorteados = document.querySelector(
-    ".quantidade-numeros"
-  ).value;
-
   const botaoSortear = document.querySelector(".sortear");
 
-  const resultado = (min, max) => {
-    console.log(min, max);
+  const resultado = (min, max, i) => {
+    const numSorteado = Math.floor(Math.random() * (max - min) + min);
+    const div = document.querySelector(".resultado");
+    const p = document.createElement("p");
+    p.innerHTML= `<p>${i+1}˚ Sorteado <span>${numSorteado}</span> </p>`
+    // `<p>${i++}˚ Sorteado <span>${numSorteado}</span> </p>`
+    div.appendChild(p);
   };
 
   document.addEventListener("click", function (e) {
@@ -14,7 +15,12 @@
     if (el.classList.contains("sortear")) {
       const numeroMinimoSorteio = document.querySelector(".min").value;
       const numeroMaximoSorteio = document.querySelector(".max").value;
-      resultado(numeroMinimoSorteio, numeroMaximoSorteio);
+      const quantidadeNumerosSorteados = Number(
+        document.querySelector(".quantidade-numeros").value
+      );
+      for (var i = 0; i !== quantidadeNumerosSorteados; i++) {
+        resultado(numeroMinimoSorteio, numeroMaximoSorteio, i);
+      }
     }
   });
 })();
