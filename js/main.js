9edeef0ebff06046fd664adf9fce95630c7b5066
Thiago Lemos <lemoos.th@gmail.com>
2021-11-15 22:12:04 -0300

    capturando do dom

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 (function () {
-})
+  const quantidadeNumerosSorteados = document.querySelector(
+    ".quantidade-numeros"
+  ).value;
+
+  const botaoSortear = document.querySelector(".sortear");
+
+  const resultado = (min, max) => {
+    console.log(min, max);
+  };
+
+  document.addEventListener("click", function (e) {
+    const el = e.target;
+    if (el.classList.contains("sortear")) {
+      const numeroMinimoSorteio = document.querySelector(".min").value;
+      const numeroMaximoSorteio = document.querySelector(".max").value;
+      resultado(numeroMinimoSorteio, numeroMaximoSorteio);
+    }
+  });
+})();
