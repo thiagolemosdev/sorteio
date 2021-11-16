9da1b0b5fec8a3dc6206956884a1ab5ba0f096d4
Thiago Lemos <lemoos.th@gmail.com>
2021-11-16 13:53:53 -0300

    Finalizado

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 (function () {
-  const botaoSortear = document.querySelector(".sortear");
+  const invalido = () => {
+    const div = document.querySelector(".resultado");
+    const h1 = document.createElement("h1");
+    h1.innerHTML = `Os numero não podem ficar em branco`;
+    div.appendChild(h1);
+  };
 
   const resultado = (min, max, i) => {
     const numSorteado = Math.floor(Math.random() * (max - min) + min);
     const div = document.querySelector(".resultado");
     const p = document.createElement("p");
-    p.innerHTML= `<p>${i+1}˚ Sorteado <span>${numSorteado}</span> </p>`
+    p.innerHTML = `${i + 1}˚ Sorteado <span>${numSorteado}</span>`;
     // `<p>${i++}˚ Sorteado <span>${numSorteado}</span> </p>`
     div.appendChild(p);
   };
@@ -18,9 +23,22 @@
       const quantidadeNumerosSorteados = Number(
         document.querySelector(".quantidade-numeros").value
       );
-      for (var i = 0; i !== quantidadeNumerosSorteados; i++) {
-        resultado(numeroMinimoSorteio, numeroMaximoSorteio, i);
+      if (
+        numeroMaximoSorteio === "" ||
+        numeroMinimoSorteio === "" ||
+        quantidadeNumerosSorteados === ""
+      ) {
+        invalido();
+      } else {
+        const div = document.querySelector(".resultado");
+        div.innerHTML = "";
+        for (var i = 0; i !== quantidadeNumerosSorteados; i++) {
+          resultado(numeroMinimoSorteio, numeroMaximoSorteio, i);
+        }
       }
+      document.querySelector(".min").value = "";
+      document.querySelector(".max").value = "";
+      document.querySelector(".quantidade-numeros").value = "";
     }
   });
 })();
